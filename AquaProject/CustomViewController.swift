c0d69ce827ddbeab91a1dcafaf237832bd5be36e
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2020-12-24 16:44:23 +0400

    push

---
+++ AquaProject/CustomViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CustomViewController.swift
+//  AquaProject
+//
+//  Created by Stanislav Terentyev on 24.12.2020.
+//
+
+import UIKit
+
+final class CustomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
