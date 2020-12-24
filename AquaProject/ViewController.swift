0f90142cc5800fa67cc0b943de998767039bc88a
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2020-12-24 16:05:14 +0400

    Initial Commit

---
+++ AquaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AquaProject
+//
+//  Created by Stanislav Terentyev on 24.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
